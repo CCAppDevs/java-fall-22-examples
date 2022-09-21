95ee434d9e416afdfd0a0f5e155dff25ff2e8e83
Jesse Harlan <gotsanity@gmail.com>
2022-09-21 09:18:27 -0700

    init

---
+++ VariableLecture/src/main/Main.java
@@ -0,0 +1,23 @@
+package main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// declared a new variable
+		int age;
+		double changingNumber = 20.5;
+		
+		changingNumber = age;
+		
+		// assigned a value to the variable
+		age = 40;
+		
+		//age = 20.5; bad code
+		
+		// read the value of a variable
+		System.out.println("My age is: " + age);
+	}
+
+}
---
+++ VariableLecture/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author gotsa
+ *
+ */
+module VariableLecture {
+}
